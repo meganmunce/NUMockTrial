ca54e2b239c233771cfb9fc8edcff4e7c3270156
meganmunce <meganmunce2022@u.northwestern.edu>
2020-08-08 00:18:05 -0700

    update12:18

---
+++ Recruitment2020/welcome.js
@@ -0,0 +1,8 @@
+const removeanimation = () => {
+  document.querySelector('.main').innerHTML = `
+  <img src="logo.PNG" class="logoimg">
+  <h2>Welcome to Northwestern Mock Trial Recruitment 2020</h2>
+<a href="homepage.html" class="enter over">Click to Enter</a>`;
+};
+
+setTimeout(removeanimation, 4000);
