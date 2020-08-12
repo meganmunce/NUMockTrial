067276c17d864c12aba592d32b3ee64ec5d4fd22
meganmunce <meganmunce2022@u.northwestern.edu>
2020-08-11 19:58:32 -0700

    changeanimation

--- Recruitment2020/homepage.js
+++ Recruitment2020/homepage.js
@@ -6,4 +6,36 @@ const removeanimation = () => {
 
 setTimeout(removeanimation, 1800);
 
- 
+learnContent = ``
+
+watchContent = ``
+
+watchCall = ``
+
+const removeLearn = () => {
+  $(".e").addClass("fadeleft");
+  console.log('added');
+};
+
+const removeWatch = () => {
+
+};
+
+const removeCall = () => {
+
+};
+
+const homepage = () => {
+
+};
+
+// e is learn more
+document.querySelector('.e').onclick = removeWatch;
+document.querySelector('.e').onclick = removeCall;
+// f is watch videos
+document.querySelector('.f').onclick = removeLearn;
+document.querySelector('.f').onclick = removeCall;
+// g is zoom calls
+document.querySelector('.g').onclick = removeLearn;
+document.querySelector('.g').onclick = removeWatch;
+// to return to homepage
