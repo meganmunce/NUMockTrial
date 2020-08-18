f45d0c5d2649bdbc41930fb145f76552f1884589
meganmunce <meganmunce2022@u.northwestern.edu>
2020-08-18 13:44:40 -0700

    0818 1:44

--- Recruitment2020/homepage.js
+++ Recruitment2020/homepage.js
@@ -1,3 +1,5 @@
+// OPENING ANIMATION
+
 const removeanimation = () => {
   $(".box").removeClass("fadedown");
   $(".u").addClass("grow");
@@ -7,6 +9,52 @@ const removeanimation = () => {
 };
 
 setTimeout(removeanimation, 1800);
+
+// MODAL TUTORIAL
+
+var modal = document.getElementById("mymodal");
+var whitemodal = document.getElementById("whitemodal");
+var logo = document.querySelector(".logoclass");
+var modal1 = document.querySelector(".modal1");
+var modal2 = document.querySelector(".modal2");
+
+const showModal = () => {
+  modal.style.display="block";
+  whitemodal.style.display="block";
+};
+
+setTimeout(showModal, 3000);
+
+const skipTutorial = () => {
+  modal.style.display="none";
+  whitemodal.style.display="none";
+};
+
+const closeModal = () => {
+  modal.style.display="none";
+  whitemodal.style.display="none";
+  logo.style.border="5px solid red";
+  modal1.style.display="block";
+};
+
+const step2 = () => {
+  logo.style.border="none";
+  modal1.style.display="none";
+  document.querySelector('.b').style.border="5px solid red";
+  modal2.style.display="block";
+};
+
+const step3 = () => {
+  modal2.style.display="none";
+  document.querySelector('.b').style.border="none";
+};
+
+document.querySelector('.modalclose').onclick = closeModal;
+document.querySelector('.skiptutorial').onclick = skipTutorial;
+document.querySelector(".next1").onclick = step2;
+document.querySelector(".next2").onclick = step3;
+
+// CHANGING CONTENT
 
 learnContent = ``
 
