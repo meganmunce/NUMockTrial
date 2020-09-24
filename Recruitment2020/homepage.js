0ca37cdfd61e402efc1d5638d9950952b72f6b61
meganmunce <meganmunce2022@u.northwestern.edu>
2020-09-24 17:11:52 -0500

    finalupdate

--- Recruitment2020/homepage.js
+++ Recruitment2020/homepage.js
@@ -25,67 +25,10 @@ const showModal = () => {
 
 setTimeout(showModal, 3000);
 
-const skipTutorial = () => {
-  modal.style.display="none";
-  whitemodal.style.display="none";
-};
-
 const closeModal = () => {
   modal.style.display="none";
   whitemodal.style.display="none";
-  logo.style.border="5px solid #BBA2E2";
   modal1.style.display="block";
 };
 
-const step2 = () => {
-  logo.style.border="none";
-  modal1.style.display="none";
-  document.querySelector('.b').style.border="5px solid #BBA2E2";
-  modal2.style.display="block";
-};
-
-const step3 = () => {
-  modal2.style.display="none";
-  document.querySelector('.b').style.border="none";
-};
-
 document.querySelector('.modalclose').onclick = closeModal;
-document.querySelector('.skiptutorial').onclick = skipTutorial;
-document.querySelector(".next1").onclick = step2;
-document.querySelector(".next2").onclick = step3;
-
-// CHANGING CONTENT
-
-learnContent = ``
-
-watchContent = ``
-
-watchCall = ``
-
-const removeLearn = () => {
-  $(".e").addClass("fadeleft");
-  console.log('added');
-};
-
-const removeWatch = () => {
-
-};
-
-const removeCall = () => {
-
-};
-
-const homepage = () => {
-
-};
-
-// e is learn more
-document.querySelector('.e').onclick = removeWatch;
-document.querySelector('.e').onclick = removeCall;
-// f is watch videos
-document.querySelector('.f').onclick = removeLearn;
-document.querySelector('.f').onclick = removeCall;
-// g is zoom calls
-document.querySelector('.g').onclick = removeLearn;
-document.querySelector('.g').onclick = removeWatch;
-// to return to homepage
